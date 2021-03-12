8574cde841c6bef53dec25f1457ad75d25431827
Alejandro <Alex21_97@hotmail.com>
2021-03-12 15:05:19 -0700

    commit alex 12/03/21

---
+++ routes/clientes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('clientes');
+})
+
+router.get('/citasDelCliente', (req, res) => {
+    res.render('citasDelUsuario')
+})
+
+
+module.exports = router
