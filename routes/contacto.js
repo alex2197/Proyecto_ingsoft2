ce7af8b38b0f56a53b6923026c53029ccb173a30
Alejandro <Alex21_97@hotmail.com>
2021-03-18 17:40:10 -0700

    El desastre de Yorge

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const User = require('../database/models/Clientes')
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('Contacto');
+})
+
+router.post('/', (req, res) => {
+    User.create({
+        firstName:"Aaron",
+        lastName:"Valadez"
+    }).then(user => {
+        res.json(user);
+    })
 })
 
 module.exports = router;
